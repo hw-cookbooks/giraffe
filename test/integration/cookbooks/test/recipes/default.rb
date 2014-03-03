ba023e662c802a10c599514c2a07f1936e5aaa6a
Anthony Goddard <anthony@anthonygoddard.com>
2014-03-03 15:25:09 -0500

    example instance

--- test/integration/cookbooks/test/recipes/default.rb
+++ test/integration/cookbooks/test/recipes/default.rb
@@ -2,6 +2,22 @@ include_recipe "apache2"
 
 giraffe_instance "/srv/www/giraffe" do
   graphite_url "http://127.0.0.1"
+  dashboards [
+    {
+      :name => "test",
+      :refresh => 5000,
+      :description => "test dashboard",
+      :metrics => [
+        {
+          :alias =>  "test",
+          :target => "metrics.test.tests",
+          :description => "Example metric",
+          :summary => "sum",
+          :summary_formatter => "d3.format(\",f\")"
+        }
+      ]
+    }
+  ]
 end
 
 template "#{node['apache']['dir']}/sites-available/giraffe" do
