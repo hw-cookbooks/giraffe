f15ac2a6c29e65b8ffb7f3a2027f83e2249b367b
Anthony Goddard <anthony@anthonygoddard.com>
2014-03-10 12:16:36 -0400

    up version for development

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Heavy Water Software Inc.'
 maintainer_email 'ops@hw-ops.com'
 license          'Apache 2.0'
 description      'Manages Giraffe, a Graphite dashboard with a long neck'
-version          '0.1.0'
+version          '0.1.1'
 
 %w(ubuntu centos).each do |os|
   supports os
